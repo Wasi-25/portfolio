430a9e3b34c5b5e722e88e6114bb08c62583b909
Wasiullah Khalique <79196849+Wasi-25@users.noreply.github.com>
2023-11-13 16:45:38 +0530

    experience updated

--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -118,6 +118,13 @@ const Experience = () => {
             <article className="experience__details">
               <BsPatchCheckFill className="experience__details-icon"/>
               <div>
+                <h4>JWT</h4>
+              </div>
+            </article>
+
+            <article className="experience__details">
+              <BsPatchCheckFill className="experience__details-icon"/>
+              <div>
                 <h4>REST API</h4>
               </div>
             </article>
